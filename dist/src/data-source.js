4053c110dedf002783699bb799f0f075bcb35b2b
FrankDev-327 <renteriafrancisco51@gmail.com>
2023-08-31 23:23:29 +0200

    added migrations

--- dist/src/data-source.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppDataSource = void 0;
-require("reflect-metadata");
-var ormconfig_1 = require("../ormconfig");
-var typeorm_1 = require("typeorm");
-exports.AppDataSource = new typeorm_1.DataSource(ormconfig_1.typeOrmConfig);
-//# sourceMappingURL=data-source.js.map
